ab58c72dcbe707bcc2342d9cbbf2d4147b173cec
Matt Helm <mthelm85@gmail.com>
2021-05-13 08:10:04 -0500

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.16

---
+++ src/SingleFactorANOVA.jl
@@ -0,0 +1,5 @@
+module SingleFactorANOVA
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using SingleFactorANOVA
+using Test
+
+@testset "SingleFactorANOVA.jl" begin
+    # Write your tests here.
+end
